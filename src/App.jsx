09d574c5e95e78e1b80790a6df27c93c3e9f7dfd
Raulm3ga <raulpedromega@gmail.com>
2023-05-30 21:46:46 -0600

    Add Update of course

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@
 import { createRoot } from 'react-dom/client';
 
 // Components
-import { Pet } from './Pet';
+import { Pet } from './components/Pet';
+import { SearchParams } from './components/SearchParams';
 
-const App = () => (
-  <div>
-    <Pet name="Terry02" animal="Cotorro" breed="croquetas" />
-  </div>
-);
+const App = () => {
+  const Pets = [
+    { id: 1, name: "Max", animal: "Perro", breed: "Croquetas" },
+    { id: 2, name: "Terry02", animal: "Cotorro", breed: "Alpiste" },
+  ];
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  )
+}
 
 const container = document.getElementById('root');
 const root = createRoot(container);
